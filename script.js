88898947036156b21601f34213075244783dc6b8
Raphael Z <raphaelzaafrani@gmail.com>
2021-12-06 16:20:59 -0800

    Added some interactions to the check button. Checking the number as well

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const goal = Math.trunc(Math.random()*20)+1
+let score = 20
+
+const decreaseScore = function(){
+    score--
+    if(score < 0){
+        document.querySelector(".message").textContent = "You lost! Better luck next time."
+    }
+    else{
+        document.querySelector('.score').textContent = score
+    }
+}
+
+document.querySelector(".check").addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value)
+
+    if(!guess){
+        document.querySelector(".message").textContent = "No input detected"
+    } else if(guess < goal){
+        document.querySelector(".message").textContent = "Too low!"
+        decreaseScore()
+    }else if(guess > goal){
+        document.querySelector(".message").textContent = "Too high!"
+        decreaseScore()
+    }else{
+        document.querySelector(".message").textContent = "🥳 Correct!"
+    }
+})
